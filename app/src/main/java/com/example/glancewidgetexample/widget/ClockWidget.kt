0dcbff371888ecd1b111966ad6261363c172e5f8
joy458963214 <90612508+joy458963214@users.noreply.github.com>
2023-03-05 23:54:54 +0800

    feat: add background

--- app/src/main/java/com/example/glancewidgetexample/widget/ClockWidget.kt
+++ app/src/main/java/com/example/glancewidgetexample/widget/ClockWidget.kt
@@ -1,16 +1,25 @@
 package com.example.glancewidgetexample.widget
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.glance.GlanceModifier
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.background
+import androidx.glance.layout.fillMaxSize
 import java.time.LocalDateTime
 
 /**
  * 處理UI
  */
-class ClockWidget: GlanceAppWidget(){
+class ClockWidget : GlanceAppWidget() {
     @Composable
     override fun Content() {
         val time = LocalDateTime.now()
-        ClockWidgetScreen(String.format("%02d:%02d", time.hour, time.minute))
+        ClockWidgetScreen(
+            String.format("%02d:%02d", time.hour, time.minute),
+            modifier = GlanceModifier.fillMaxSize().background(
+                Color.White
+            )
+        )
     }
 }
--- app/src/main/java/com/example/glancewidgetexample/widget/ClockWidgetScreen.kt
+++ app/src/main/java/com/example/glancewidgetexample/widget/ClockWidgetScreen.kt
@@ -1,9 +1,19 @@
 package com.example.glancewidgetexample.widget
 
 import androidx.compose.runtime.Composable
+import androidx.glance.GlanceModifier
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Row
 import androidx.glance.text.Text
 
 @Composable
-fun ClockWidgetScreen(time: String) {
-    Text(text = time)
+fun ClockWidgetScreen(time: String, modifier: GlanceModifier) {
+    Row(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Text(text = time)
+    }
+
 }
